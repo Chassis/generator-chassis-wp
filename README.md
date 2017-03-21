20c469d4457a9000b8422510126223cc90e47d54
Brad Parbs <brad@bradparbs.com>
2017-03-21 11:57:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Chassis Yeoman Generator
+> Generate a new [Chassis](http://chassis.io) project.
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-chassis-wp using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-chassis-wp
+```
+
+Then generate your new project and follow the prompts.
+
+```bash
+yo chassis-wp
+```
+## What happens during installation?
+
+When running the generator, you will be prompted for
+
+ * Project name. Used for folder name where the project gets scaffolded.
+ * PHP Version. 
+ * If multisite should be enabled.
+ * Which [extensions](http://docs.chassis.io/en/latest/extend/) to automatically enable. 
+
+ All options, except for project name, will be saved as the default for the next generation you do.
+
+## Options
+
+| Flag             | Usage                          | Description                                                    |
+| ---------------- |------------------------------- | -------------------------------------------------------------- |
+| `--skip-vagrant` | `yo chassis-wp --skip-vagrant` |  Will skip bringing newly created project up via Vagrant.      |
+| `--defaults`     | `yo chassis-wp --defaults`     |  Will skip all options except name and use the saved defaults. |
---
+++ generators/app/index.js
@@ -0,0 +1,189 @@
+'use strict';
+var Generator = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var slugify = require('underscore.string/slugify');
+
+module.exports = Generator.extend({
+  prompting: function () {
+    this.log(yosay(
+      'Welcome to the ' + chalk.red('generator-chassis-wp') + ' generator!'
+    ));
+
+    // Allow skipping vagrant with a flag.
+    this.option('skip-vagrant', {
+      type: Boolean,
+      required: false,
+      desc: 'Skip vagrant up after creation.'
+    });
+
+    // Allow just using our defaults with a flag.
+    this.option('defaults', {
+      type: Boolean,
+      required: false,
+      desc: 'Use all defaults for optional paramaters.'
+    });
+
+    var prompts = [{
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?',
+      default: this.appname
+    }, //{
+    //   type: 'input',
+    //   name: 'host',
+    //   message: 'What url would you like to use?',
+    //   default: 'vagrant.local'
+    // },
+    {
+      type: 'list',
+      name: 'phpVersion',
+      message: 'What version of PHP would you like to run?',
+      default: '7.1',
+      store: true,
+      when: ! this.options.defaults,
+      choices: [
+        '5.3',
+        '5.4',
+        '5.5',
+        '5.6',
+        '7.0',
+        '7.1'
+      ]
+    }, {
+      type: 'list',
+      name: 'multisite',
+      message: 'Enable multisite?',
+      default: 'No',
+      store: true,
+      when: ! this.options.defaults,
+      choices: [{
+        name:  'No',
+        value: 'No',
+      }, {
+        name:  'Yes, with subfolders.',
+        value: 'Yes',
+      }, {
+        name:  'Yes, with subdomains.',
+        value: 'subdomains',
+      }
+      ]
+    }, {
+      type: 'checkbox',
+      name: 'extensions',
+      message: 'Which additional extensions would you like to include?',
+      store: true,
+      when: ! this.options.defaults,
+      choices: [{
+        name: 'Xdebug',
+        value: 'chassis/Xdebug',
+        checked: true,
+      }, {
+        name: 'phpcs',
+        value: 'chassis/phpcs',
+        checked: true,
+      }, {
+        name: 'nodejs',
+        value: 'chassis/nodejs',
+        checked: true,
+      }, {
+        name: 'Fish',
+        value: 'chassis/Fish',
+        checked: true,
+      }, {
+        name: 'Composer',
+        value: 'chassis/Composer',
+        checked: true,
+      }, {
+        name: 'Tester',
+        value: 'chassis/Tester',
+        checked: true,
+      }, {
+        name: 'MailHog',
+        value: 'chassis/MailHog',
+        checked: false,
+      }, {
+        name: 'SequelPro',
+        value: 'chassis/SequelPro',
+        checked: false,
+      }, {
+        name: 'Cavalcade',
+        value: 'chassis/Cavalcade',
+        checked: false,
+      }, {
+        name: 'Theme Review',
+        value: 'chassis/ThemeReview',
+        checked: false,
+      }, {
+        name: 'xhprof',
+        value: 'chassis/xhprof',
+        checked: false,
+      }, {
+        name: 'Debugging',
+        value: 'chassis/Debugging',
+        checked: false,
+      }, {
+        name: 'phpMyAdmin',
+        value: 'chassis/phpMyAdmin',
+        checked: false,
+      }, {
+        name: 'Query-Monitor',
+        value: 'chassis/Query-Monitor',
+        checked: false,
+      }, {
+        name: 'memcache',
+        value: 'chassis/memcache',
+        checked: false,
+      }, {
+        name: 'db-backup',
+        value: 'chassis/db-backup',
+        checked: false,
+      }, {
+        name: 'local-dev',
+        value: 'chassis/local-dev',
+        checked: false,
+      }]
+    }];
+
+    return this.prompt(prompts).then(function (props) {
+      props.safename = slugify( props.name );
+
+      // If we're using our defaults, then set those.
+      if ( ! this.options.defaults ) {
+       props.phpVersion = this.config.get('phpVersion');
+       props.multisite  = this.config.get('multisite');
+       props.extensions = this.config.get('extensions');
+      }
+
+      this.props = props;
+    }.bind(this));
+  },
+
+  writing: function () {
+
+    // Clone chassis from Github.
+    this.spawnCommandSync('git', [ 'clone',
+      'https://github.com/Chassis/Chassis', this.props.safename,
+      '--depth', '1',
+    ]);
+
+    // Copy over our yaml config.
+    this.fs.copyTpl(
+      this.templatePath('_config.local.yaml'),
+      this.destinationPath(this.props.safename + '/config.local.yaml'),
+      {
+        name: this.props.name,
+        phpVersion: this.props.phpVersion,
+        extensions: this.props.extensions
+      }
+    );
+  },
+
+  install: function () {
+
+    // If we didn't flag to skip vagrant, then do a vagrant up.
+    if ( ! this.options['skip-vagrant'] ) {
+      this.spawnCommand('vagrant', ['up'], { cwd: this.props.safename } );
+    }
+  }
+});
